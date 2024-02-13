08c539a597f7c7057fe76b85a92119055798d1be
Eslam Adel <eadel@lucidya.com>
2024-02-13 09:38:16 +0200

    finish ex5

---
+++ ex5.rb
@@ -0,0 +1,18 @@
+my_name = 'Eslam Adel'
+my_age = 25 # not a lie in 1998
+my_height = 170 # inches
+my_weight = 70 # kg
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} kg heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+# the result of this line is 25 + 170 + 70 = 265
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
